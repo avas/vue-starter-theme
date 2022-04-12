aa35c7e8f04157fcc813a24d3acd5b330c1a2067
Anton Veretennikov <superiorboroda@gmail.com>
2022-04-12 18:51:28 +0300

    feat: setup locale at app startup by locatoin path. add base Url to links
    load products menu with current locale

--- client-app/core/api/graphql/common/queries/getMenus/index.ts
+++ client-app/core/api/graphql/common/queries/getMenus/index.ts
@@ -1,14 +1,14 @@
 import client from "@core/api/graphql/graphql-client";
 import { MenuLinkListType } from "@core/api/graphql/types";
-import { locale, storeId } from "@core/constants";
+import { storeId } from "@core/constants";
 import getMenusQueryDocument from "./getMenus.graphql";
 
-export default async function getMenus(): Promise<MenuLinkListType[]> {
+export default async function getMenus(cultureName: string): Promise<MenuLinkListType[]> {
   const { data } = await client.query({
     query: getMenusQueryDocument,
     variables: {
       storeId: storeId,
-      cultureName: locale,
+      cultureName,
     },
   });
 
