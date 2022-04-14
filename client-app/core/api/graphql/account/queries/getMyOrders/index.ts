1a4d937abf36fc509ae6f0f458ddd03e3740087a
Andrey A. Vasenev <avas.the.wise@gmail.com>
2022-04-14 19:21:19 +0700

    refactor: fix build failure due to order connection type name change

--- client-app/core/api/graphql/account/queries/getMyOrders/index.ts
+++ client-app/core/api/graphql/account/queries/getMyOrders/index.ts
@@ -1,9 +1,9 @@
 import client from "@core/api/graphql/graphql-client";
-import { CustomerOrderConnection, Query, QueryOrdersArgs } from "@core/api/graphql/types";
+import { TrainingOrderConnection, Query, QueryOrdersArgs } from "@core/api/graphql/types";
 import getMyOrdersQueryDocument from "./getMyOrdersQuery.graphql";
 import { currentUserId, locale } from "@core/constants";
 
-export default async function getMyOrders(payload?: QueryOrdersArgs): Promise<CustomerOrderConnection> {
+export default async function getMyOrders(payload?: QueryOrdersArgs): Promise<TrainingOrderConnection> {
   const { data } = await client.query<Required<Pick<Query, "orders">>, QueryOrdersArgs>({
     query: getMyOrdersQueryDocument,
     variables: {
